55088841b69572db2101b45e07a79cdc53054613
Oyembo Peninah <aumaoyembo@gmail.com>
2025-09-05 14:03:45 +0300

    Add recent file changes

--- app/views.py
+++ app/views.py
@@ -1,45 +1,18 @@
-from django.shortcuts import render
-from .models import User, Tenant, Caretaker, Apartment, ApartmentImage
-from django.forms import ModelForm, UserForm, ApartmentForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
- def user_create(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-
-            if user.user_type == 'Tenant':
-                Tenant.objects.create(email=user)
-            elif user.user_type == 'Caretaker':
-                Caretaker.objects.create(email=user)
-            return redirect('user_list')
-
-    else:
-        form = UserForm()
-    return render(request, 'user_create.html', {'form': form})
-
-def apartment_create(request):
-    if request.method == 'POST':
-        form = ApartmentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('apartment_list')
-    else:
-        form = ApartmentForm()
-    return render(request, 'apartment_create.html', {'form': form})
-
-def apartment_detail(request, apartment_code):
-    apartment = get_object(Apartment, apartment_code=apartment_code)
-    images = ApartmentImage.objects.filter(apartment_code=apartment)
-    return render(request, 'apartment_detail.html', {'apartment': apartment, 'images': images})
+from .models import User, Tenant, Caretaker, Apartment, ApartmentImage
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['name', 'email', 'user_type']
+
+class ApartmentForm(ModelForm):
+    class Meta:
+        model = Apartment
+        fields = ['name', 'description', 'apartment_code', 'location_name', 'road', 'bedrooms', 'rent', 'caretaker']
 
 def register(request):
     if request.method == 'POST':
@@ -69,11 +42,6 @@ def user_create(request):
     else:
         form = UserForm()
     return render(request, 'user_create.html', {'form': form})
-
-class ApartmentForm(ModelForm):
-    class Meta:
-        model = Apartment
-        fields = ['name', 'description', 'apartment_code', 'location_name', 'road', 'bedrooms', 'rent', 'caretaker']
 
 @login_required
 def apartment_create(request):
