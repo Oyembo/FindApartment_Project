0b6ed51df7da246d76625aa52bb5a67199632801
Oyembo Peninah <aumaoyembo@gmail.com>
2025-09-06 14:45:40 +0300

    Add all file changes

--- apartment_finder_project/urls.py
+++ apartment_finder_project/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.api_views import UserViewSet, CaretakerViewSet, TenantViewSet, ApartmentViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'caretakers', CaretakerViewSet)
+router.register(r'tenants', TenantViewSet)
+router.register(r'apartments', ApartmentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('', include('app.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
